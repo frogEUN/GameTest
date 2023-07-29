1ebc3601f7052464ee92a7c02cca21762069e97f
frogEUN <eunhasu801@gmail.com>
2023-07-29 11:46:00 +0900

    Drag

---
+++ Assets/ForInventory/DragAndDrop/SlotForDrag.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SlotForDrag : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    public Vector2 startPos;
+
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        startPos = this.transform.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        this.transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        this.transform.position = startPos;
+    }
+}
