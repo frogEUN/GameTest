0a7f9ad49f366c251a03e5cae29beb66ec3ad949
frogEUN <eunhasu801@gmail.com>
2023-08-01 10:01:33 +0900

    ondo

--- Assets/ForTimeFlow/TimeFlow.cs
+++ Assets/ForTimeFlow/TimeFlow.cs
@@ -12,6 +12,7 @@ public class TimeFlow : MonoBehaviour
     public TextMeshProUGUI viewTimeText;
     private int day = 1;
     public Image ondo;
+    public float timeFlow = 0.001f;
 
     void Start()
     {
@@ -24,22 +25,35 @@ public class TimeFlow : MonoBehaviour
     void FixedUpdate()
     {
         
-        color.a += 0.001f * minus;
+        color.a += timeFlow * minus;
         image.color = color;
-        ondo.rectTransform.sizeDelta = new Vector2(36.258f, 130f - color.a * 100);
+
+        ondo.rectTransform.sizeDelta = new Vector2(14f, 130f - color.a * 100);
         // 변하는 알파값을 이용하여 온도 이미지 크기 줄이기
 
         if(color.a <= 0)
         {
             minus *= -1;
         }
-        if (color.a >= 0.8f)
+        if (color.a >= 0.6f)
         {
             minus *= -1;
             day += 1;
             viewTimeText.text = "Day - " + day;
-
         }
+        if(color.a >= 0 || color.a <= 0.1f)
+        {  // 낮 지속
+            timeFlow = 0.002f;
+        }
+        else if(color.a <= 0.6f || color.a >= 0.5f)
+        {  // 밤 지속
+            timeFlow = 0.008f;
+        }
+        else
+        {
+            timeFlow = 0.001f;
+        }
+
 
     }
 }
